e5fa323bddc9484aa5e94cf00843a33be531817f
chairat chaikong <chairat.chaikong@gmail.com>
2024-10-19 20:46:21 +0700

    feat: add exp section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,20 @@
 import React from 'react'
 import About from '../components/Section/About'
 import ReactPageScroller, { SectionContainer } from 'react-page-scroller'
+import Experience from '@/components/Section/Experience'
+
 const Home = () => {
   return (
     <div>
       <ReactPageScroller>
-        <SectionContainer height={85}>
+        <SectionContainer height={100}>
           <About />
         </SectionContainer>
-        <SectionContainer height={85}>
-          page 2
+        <SectionContainer height={100}>
+          <Experience />
+        </SectionContainer>
+        <SectionContainer height={100}>
+          page 3
         </SectionContainer>
       </ReactPageScroller>
     </div>
